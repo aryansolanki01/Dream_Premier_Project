d513280bd6e2befa879d6d66e7aad2b6f4205cc5
Aryan Solanki <aryansolanki0418@gmail.com>
2024-12-23 18:51:16 +0530

    Solve Problem 1

---
+++ my_solution.py
@@ -0,0 +1,12 @@
+# A match of DPL is going on. Team Aravali has a made score of 195 for 7 in 20 overs and the opponent Team Shivalik is doing some analytics to find out what they need to do to win. You are the Data Scientist of the Team Shivalik and you have been asked to help them.
+
+# Problem 1
+# Team Shivalik has stored the runs made by Team Aravali players in the following dictionary.
+
+aravali ={ "Dhoni":25, "Virat":31, "Pollard":11, "Rohit": 0, "Maxwell":12, "Sachin":59, "Sehwag":12 }
+
+# Get the list of all players in Team Aravali
+
+players_list = [p for p in aravali]
+print(players_list)
+
